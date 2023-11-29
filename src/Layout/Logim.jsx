2012e94cc8f836bafe213a6b47f840e127a5c631
Rahat Hasan <devrahat.hasan@gmail.com>
2023-11-29 19:09:04 +0600

    authentication

--- src/Layout/Logim.jsx
+++ src/Layout/Logim.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from '../firebase.init';
 
 const Logim = () => {
   const auth =getAuth(app);
+  console.log(app);
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = ()=>{
-    console.log('google mama is coming');
+    signInWithPopup(auth,provider)
+    .then(
+      result => {
+        const user =result.user;
+        console.log(user);
+      }
+    )
+    .catch(error=>{
+      console.log('error',error.message);
+    })
   }
   return (
     <div>
